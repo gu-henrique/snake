a87acb3ca3d7b782a4a1cee1d26cccb579f8c055
Gustavo Henrique <104112939+gu-henrique@users.noreply.github.com>
2023-02-24 10:10:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,16 +25,16 @@ const handleGameOver = () => {
 }
 const changeDirection = e => {
     // Changing velocity value based on key press
-    if(e.key === "ArrowUp" && velocityY != 1) {
+    if(e.key === "ArrowUp" && velocityY != 1.5) {
         velocityX = 0;
         velocityY = -1.5;
-    } else if(e.key === "ArrowDown" && velocityY != -1) {
+    } else if(e.key === "ArrowDown" && velocityY != -1.5) {
         velocityX = 0;
         velocityY = 1.5;
-    } else if(e.key === "ArrowLeft" && velocityX != 1) {
+    } else if(e.key === "ArrowLeft" && velocityX != 1.5) {
         velocityX = -1.5;
         velocityY = 0;
-    } else if(e.key === "ArrowRight" && velocityX != -1) {
+    } else if(e.key === "ArrowRight" && velocityX != -1.5) {
         velocityX = 1.5;
         velocityY = 0;
     }
